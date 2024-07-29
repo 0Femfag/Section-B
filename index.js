0aad30b25d3aaecf605ef73fb28932a5a282c41b
0Femfag <Femimane1@gmail.com>
2024-07-29 20:02:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const myStudent = ["Femi", "Monday", "Emmanuel", "Victoria", "David", "Joseph", "Ade", "Samuel", "Francis", "Linda"];
+const firstPerson = myStudent[0];
+const secondPerson = myStudent[1];
+const thirdPerson = myStudent[2];
+const forthPerson = myStudent[3];
+const fifthPerson = myStudent[4];
+const sixthPerson = myStudent[5];
+const seventhPerson = myStudent[6];
+const eighthPerson = myStudent[7];
+const ninthPerson = myStudent[8];
+const tenthPerson = myStudent[9];
+for (i = 0; i < 10; i++){
+    const currentItem = myStudent[i];
+    console.log(currentItem);
+};
+
+const property = { name: "Oluwafemi", age: 77, married: false, location: undefined };
+const getmyName = property.name;
+const getmyAge = property.age;
+const getMarried = property.married;
+const getLocation = property.location;
+console.log(getmyName);
+console.log(getmyAge);
+console.log(getMarried);
+console.log(getLocation);
+
+
+
